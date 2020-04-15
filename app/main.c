c3a7e9639bd9cfa98c6bf79aa8ecbf7c48ac0e2f
Omar Chammaa <chammaa.omar@gmail.com>
2020-04-15 19:50:07 -0400

    refactored; setup_run_[child|parent] takes ptr to void (custom Args struct); everything seems working

--- app/main.c
+++ app/main.c
@@ -11,34 +11,30 @@
 #include <sched.h>
 #include <sys/mman.h>
 #include <sys/types.h>
+#include <curl/curl.h>
 
 #define STACK_SIZE (1024 * 1024)
 #define BUFFER_SIZE 1024
 
-#ifndef CHILD_SIG
-#define CHILD_SIG SIGUSR1
-
-#endif
+typedef struct Args {
+	int* err_pipe;
+	int* out_pipe;
+	char** exec_opts;
+	pid_t child_pid;
+} Args;
 
 char* DOCKER_IMG = "/usr/local/bin/docker-explorer";
-
-struct ChildArgs {
-	int* pipe_args;
-	char** exec_args;
-};
 
 void error(char* msg) {
 	fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 	exit(1);
 }
 
-int setup_run_child(void* setup_args) {
-	// TODO: Make variadic
-	struct ChildArgs* child_args_ptr = (struct ChildArgs*) setup_args;
-	int* pipe_args = child_args_ptr->pipe_args;
-	char** exec_args = child_args_ptr->exec_args;
-	int fd_outpipe = pipe_args[0];
-	int fd_errpipe = pipe_args[1];
+int setup_run_child(void* args) {
+	Args* args_struct = (Args*) args;
+	char** exec_args = args_struct->exec_opts;
+	int fd_outpipe = args_struct->out_pipe[1];
+	int fd_errpipe = args_struct->err_pipe[1];
 	// only executing docker-explorer command for now
 	char* command = exec_args[0] = "/docker-explorer";
 	
@@ -47,28 +43,28 @@ int setup_run_child(void* setup_args) {
 	dup2(fd_errpipe, fileno(stderr));
 
 	if (execv(command, exec_args) == -1) {
-		free(setup_args);
+		free(args);
 		error("Error executing command");
 	}
 
 }
 
-int setup_run_parent(int* setup_args) {
-	// TODO: Make variadic
+int setup_run_parent(void* args) {
+	Args* args_struct = (Args*) args;
 	int status;
 	char child_err[BUFFER_SIZE] = {'\0'};
 	char child_out[BUFFER_SIZE] = {'\0'};
 
-	int fd_outpipe = setup_args[0];
-	int fd_errpipe = setup_args[1];
+	int fd_outpipe = args_struct->out_pipe[0];
+	int fd_errpipe = args_struct->err_pipe[0];
 
-	pid_t child_pid = (pid_t) setup_args[2];
+	pid_t child_pid = args_struct->child_pid;
 
 	// non-blocking IO
 	fcntl(fd_outpipe, F_SETFL, O_NONBLOCK);
 	fcntl(fd_errpipe, F_SETFL, O_NONBLOCK);
 
-	waitpid(child_pid, &status, __WALL | WUNTRACED);
+	waitpid(child_pid, &status, __WALL);
 
 	size_t out_sz = read(fd_outpipe, child_out, BUFFER_SIZE - 1);
 	size_t err_sz = read(fd_errpipe, child_err, BUFFER_SIZE - 1);
@@ -109,12 +105,7 @@ void chroot_into_tmp(const char * tmp_dir) {
 	}
 }
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-int main(int argc, char *argv[]) {
-	// Disable output buffering
-	setbuf(stdout, NULL);
-
-	chroot_into_tmp("tmp-cage/");
+Args* args_make(char* cmd_args[]) {
 
 	int fd_outpipe[2];
 	int fd_errpipe[2];
@@ -126,14 +117,15 @@ int main(int argc, char *argv[]) {
 		error("Error creating the stderr pipe");
 	}
 
-	int child_setup_args[] = {fd_outpipe[1], fd_errpipe[1]};
-	int parent_setup_args[] = {fd_outpipe[0], fd_errpipe[0], 0};
+	Args* args = (Args*) malloc(sizeof(Args));
+	args->err_pipe = fd_errpipe;
+	args->out_pipe = fd_outpipe;
+	args->exec_opts = cmd_args;
 
-	struct ChildArgs* child_args = (struct ChildArgs*) malloc(sizeof(struct ChildArgs));
+	return args;
+}
 
-	child_args->pipe_args = child_setup_args;
-	child_args->exec_args = argv + 3;
-
+pid_t clone_child(int fn (void*), void* args) {
 	char* stack = mmap(NULL, STACK_SIZE,
 					   PROT_READ | PROT_WRITE,
 					   MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK,
@@ -142,23 +134,29 @@ int main(int argc, char *argv[]) {
 	// stack grows downward
 	char* stack_top = stack + STACK_SIZE;
 
-	pid_t child_pid = clone(setup_run_child, stack_top, CLONE_NEWPID | CHILD_SIG, (void*) child_args);
+	int child_pid = clone(setup_run_child, stack_top, CLONE_NEWPID, (void*) args);
 
-	
 	if (child_pid == -1) {
-		error("Error cloning");
+		free(args);
+		error("Error cloning child process");
 	}
 
-	// if (!child_pid) {
-	// 	// Replace current program with calling program
-	// 	// this always exits; success or failure
-	// 	setup_run_child(child_setup_args, argv + 3);
-	// }
+	return child_pid;
+}
+
+// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+int main(int argc, char *argv[]) {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+
+	chroot_into_tmp("tmp-cage/");
 
 	// We're in parent
-	parent_setup_args[2] = (int) child_pid;
-	int exit_code = setup_run_parent(parent_setup_args);
+	Args* args = args_make(argv + 3);
 
-	free(child_args);
+	args->child_pid = clone_child(setup_run_child, (void*) args);
+	int exit_code = setup_run_parent(args);
+
+	free(args);
 	return exit_code;
 }
